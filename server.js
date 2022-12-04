2fe1c97073029103f470cfa6a062f95068580ae2
diegoale714 <diegodepas300@gmail.com>
2022-12-04 12:54:31 -0300

    url

--- server.js
+++ server.js
@@ -1,55 +1,50 @@
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const http = require('http');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const http = require('http')
+require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
 require('./connection')
-const server = http.createServer(app);
-const {Server} = require('socket.io');
+const server = http.createServer(app)
+const { Server } = require('socket.io')
 const io = new Server(server, {
-  cors: 'https://front-vercel-wheat.vercel.app',
-  methods: ['GET', 'POST', 'PATCH', "DELETE"]
+  cors: 'https://front-vercel-two.vercel.app',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 })
 
+const User = require('./models/User')
+const userRoutes = require('./routes/userRoutes')
+const productRoutes = require('./routes/productRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+const imageRoutes = require('./routes/imageRoutes')
 
-const User = require('./models/User');
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const imageRoutes = require('./routes/imageRoutes');
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/users', userRoutes)
+app.use('/products', productRoutes)
+app.use('/orders', orderRoutes)
+app.use('/images', imageRoutes)
 
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-app.use('/images', imageRoutes);
-
-
-
-
-app.post('/create-payment', async(req, res)=> {
-  const {amount} = req.body;
-  console.log(amount);
+app.post('/create-payment', async (req, res) => {
+  const { amount } = req.body
+  console.log(amount)
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: 'usd',
-      payment_method_types: ['card']
-    });
+      payment_method_types: ['card'],
+    })
     res.status(200).json(paymentIntent)
   } catch (e) {
-    console.log(e.message);
-    res.status(400).json(e.message);
-   }
+    console.log(e.message)
+    res.status(400).json(e.message)
+  }
 })
 
-const port = process.env.PORT || 3000 ;
-server.listen(port, ()=> {
+const port = process.env.PORT || 3000
+server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
 
-app.set('socketio', io);
+app.set('socketio', io)
